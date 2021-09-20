8d15200dae13bdc2a2dac0987b5ee2afd1ff7026
luis <luis@luis-VirtualBox>
2021-09-20 23:45:11 +0200

    Constructores y primeros metodos hechos

--- prog3-ImperialCommander/src/model/Coordinate.java
+++ prog3-ImperialCommander/src/model/Coordinate.java
@@ -1,5 +1,24 @@
 package model;
 
 public class Coordinate {
-
+	
+	private int x;
+	private int y;
+	
+	public Coordinate(int x, int y) {
+		this.x = x;
+		this.y = y;
+		
+	}
+	public Coordinate(final Coordinate c) {
+		x = c.x;
+		y = c.y;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	
 }
