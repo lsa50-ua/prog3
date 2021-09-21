9769138abb6d6d974b6ed8ce68b482bb0308aa2c
luis <luis@luis-VirtualBox>
2021-09-21 21:22:31 +0200

    finalizada la clase Coordinate

--- prog3-ImperialCommander/src/model/Coordinate.java
+++ prog3-ImperialCommander/src/model/Coordinate.java
@@ -38,5 +38,30 @@ public class Coordinate {
 		
 		return "[" + x + "," + y + "]";
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Coordinate)) {
+			return false;
+		}
+		Coordinate other = (Coordinate) obj;
+		if (x != other.x) {
+			return false;
+		}
+		if (y != other.y) {
+			return false;
+		}
+		return true;
+	}
 	
 }
