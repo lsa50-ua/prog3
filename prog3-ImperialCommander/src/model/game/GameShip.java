1ea22144b4332d4deee8fa598d04cd724b04a0de
luis <luis@luis-VirtualBox>
2021-12-05 01:23:41 +0100

    New Changes

--- prog3-ImperialCommander/src/model/game/GameShip.java
+++ prog3-ImperialCommander/src/model/game/GameShip.java
@@ -1,8 +1,16 @@
 package model.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Board;
+import model.Coordinate;
 import model.Fighter;
 import model.Ship;
 import model.Side;
+import model.exceptions.FighterAlreadyInBoardException;
+import model.exceptions.FighterNotInBoardException;
+import model.exceptions.OutOfBoundsException;
 import model.game.exceptions.WrongFighterIdException;
 
 public class GameShip extends Ship{
@@ -30,6 +38,44 @@ public class GameShip extends Ship{
 			throw new WrongFighterIdException(id);
 		}
 		return buscado;
+	}
+	public List<Integer> getFightersId(String where){
+		List<Integer> ids = new ArrayList<>();
+		
+		if(where == "board") {
+			for(Fighter f: fleet) {
+				if(f.isDestroyed() == false && f.getPosition() != null) {
+					ids.add(f.getId());
+				}
+			}
+		}
+		else {
+			if(where == "ship") {
+				for(Fighter f: fleet) {
+					if(f.isDestroyed() == false && f.getPosition() == null) {
+						ids.add(f.getId());
+					}
+				}
+			}
+			else {
+				for(Fighter f: fleet) {
+					if(f.isDestroyed() == false) {
+						ids.add(f.getId());
+					}
+				}
+			}
+		}
+		return ids;
+	}
+	public void launch(int id, Coordinate c, Board b) throws FighterAlreadyInBoardException, OutOfBoundsException, WrongFighterIdException{
+		b.launch(c, getFighter(id));
+	}
+	public void patrol(int id, Board b) throws FighterNotInBoardException, WrongFighterIdException{
+		b.patrol(getFighter(id));
+	}
+	public void improveFighter(int id, int qty, Board b) throws WrongFighterIdException {
+		Fighter f = getFighter(id);
+		if(b.inside(f.getPosition()))
 	}
 	
 }
